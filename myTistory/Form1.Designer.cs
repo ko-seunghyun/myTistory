c5bd9baf2347616ef854dd5f83f71048a4ec8fd6
ko-seunghyun <ccurian86@gmail.com>
2017-02-22 17:48:07 +0900

    초기 커밋.

---
+++ myTistory/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace myTistory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_open = new System.Windows.Forms.Button();
+            this.txb_path = new System.Windows.Forms.TextBox();
+            this.btn_upload = new System.Windows.Forms.Button();
+            this.btn_auth = new System.Windows.Forms.Button();
+            this.axWebBrowser1 = new AxSHDocVw.AxWebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.axWebBrowser1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(12, 12);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(75, 23);
+            this.btn_open.TabIndex = 0;
+            this.btn_open.Text = "파일열기";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // txb_path
+            // 
+            this.txb_path.Location = new System.Drawing.Point(93, 12);
+            this.txb_path.Name = "txb_path";
+            this.txb_path.Size = new System.Drawing.Size(637, 21);
+            this.txb_path.TabIndex = 1;
+            // 
+            // btn_upload
+            // 
+            this.btn_upload.Location = new System.Drawing.Point(12, 41);
+            this.btn_upload.Name = "btn_upload";
+            this.btn_upload.Size = new System.Drawing.Size(75, 23);
+            this.btn_upload.TabIndex = 2;
+            this.btn_upload.Text = "업로드";
+            this.btn_upload.UseVisualStyleBackColor = true;
+            // 
+            // btn_auth
+            // 
+            this.btn_auth.Location = new System.Drawing.Point(12, 70);
+            this.btn_auth.Name = "btn_auth";
+            this.btn_auth.Size = new System.Drawing.Size(75, 23);
+            this.btn_auth.TabIndex = 3;
+            this.btn_auth.Text = "인증";
+            this.btn_auth.UseVisualStyleBackColor = true;
+            this.btn_auth.Click += new System.EventHandler(this.btn_auth_Click);
+            // 
+            // axWebBrowser1
+            // 
+            this.axWebBrowser1.Enabled = true;
+            this.axWebBrowser1.Location = new System.Drawing.Point(93, 70);
+            this.axWebBrowser1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWebBrowser1.OcxState")));
+            this.axWebBrowser1.Size = new System.Drawing.Size(637, 265);
+            this.axWebBrowser1.TabIndex = 4;
+            this.axWebBrowser1.DocumentComplete += new AxSHDocVw.DWebBrowserEvents2_DocumentCompleteEventHandler(this.axWebBrowser1_DocumentComplete);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 347);
+            this.Controls.Add(this.axWebBrowser1);
+            this.Controls.Add(this.btn_auth);
+            this.Controls.Add(this.btn_upload);
+            this.Controls.Add(this.txb_path);
+            this.Controls.Add(this.btn_open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWebBrowser1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.TextBox txb_path;
+        private System.Windows.Forms.Button btn_upload;
+        private System.Windows.Forms.Button btn_auth;
+        private AxSHDocVw.AxWebBrowser axWebBrowser1;
+    }
+}
+
---
+++ myTistory/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.OneNote;
+using System.Xml.Linq;
+using System.Net;
+using System.IO;
+
+namespace myTistory
+{
+    public partial class Form1 : Form
+    {
+        private string srcPath = "";
+        private string dstPath = "";
+
+        private string AuthURL = "https://www.tistory.com/oauth/authorize";
+        private string BlogInfoURL = "https://www.tistory.com/apis/blog/info?access_token=";
+
+        private string DELIM_ACC_TOK = "#access_token";
+        private string DELIM_STAT = "&state=";
+        private string ACCESS_TOKEN = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            ofd.Filter = "원노트 파일 (*.one)|*.one";//추후지원 word
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                srcPath = txb_path.Text = ofd.FileName;
+                dstPath = srcPath.Replace(srcPath.Substring(srcPath.LastIndexOf(".")), ".mht");
+            }
+        }
+
+        public void oneToMht()
+        {
+            Microsoft.Office.Interop.OneNote.Application onenoteApp = new Microsoft.Office.Interop.OneNote.Application();
+
+            string sectionId;
+            onenoteApp.OpenHierarchy(srcPath, null, out sectionId);
+
+            try
+            {
+                onenoteApp.Publish(sectionId, dstPath, Microsoft.Office.Interop.OneNote.PublishFormat.pfMHTML, "");
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void btn_auth_Click(object sender, EventArgs e)
+        {
+            StringBuilder dataParams = new StringBuilder();
+            dataParams.Append("client_id=74e30b40c4ffe56b9e6b1b016575c2bd&");
+            dataParams.Append("redirect_uri=http://fallingstar.tistory.com/&");
+            dataParams.Append("response_type=token");
+            //dataParams.Append("response_type=code");
+
+            
+            axWebBrowser1.Navigate(AuthURL + "?" + dataParams);
+
+
+            /* POST */
+            // HttpWebRequest 객체 생성, 설정
+            /*HttpWebRequest request = (HttpWebRequest)WebRequest.Create(strUri);
+            request.Method = "POST";    // 기본값 "GET"
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteDataParams.Length;
+            
+             
+            // 요청 String -> 요청 Byte 변환
+            byte[] byteDataParams = UTF8Encoding.UTF8.GetBytes(dataParams.ToString());
+
+            // 요청 Byte -> 요청 Stream 변환
+            Stream stDataParams = request.GetRequestStream();
+            stDataParams.Write(byteDataParams, 0, byteDataParams.Length);
+            stDataParams.Close();
+             */
+
+            /* GET */
+            // GET 방식은 Uri 뒤에 보낼 데이터를 입력하시면 됩니다.
+            /*HttpWebRequest request = (HttpWebRequest)WebRequest.Create(AuthURL + "?" + dataParams);
+            request.Method = "GET";
+
+
+            // 요청, 응답 받기
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                // 응답 Stream 읽기
+                Stream stReadData = response.GetResponseStream();
+                StreamReader srReadData = new StreamReader(stReadData, Encoding.Default);
+
+                // 응답 Stream -> 응답 String 변환
+                string strResult = srReadData.ReadToEnd();
+
+                Console.WriteLine(strResult);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                string t = ex.StackTrace;
+            }*/
+
+
+        }
+
+        private void axWebBrowser1_DocumentComplete(object sender, AxSHDocVw.DWebBrowserEvents2_DocumentCompleteEvent e)
+        {
+            string url;
+
+            url = e.uRL.ToString();
+
+            int idx = url.IndexOf("#access_token");
+
+            //엑세스 토큰 받아옴.
+            if (idx > 0)
+            {
+                string temp = url.Substring(idx + DELIM_ACC_TOK.Length + 1);
+
+                ACCESS_TOKEN = temp.Substring(0, temp.Length - DELIM_STAT.Length);
+
+                StringBuilder dataParams = new StringBuilder();
+                dataParams.Append(ACCESS_TOKEN);
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(BlogInfoURL + dataParams);
+                request.Method = "GET";
+
+
+                // 요청, 응답 받기
+                try
+                {
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                    // 응답 Stream 읽기
+                    Stream stReadData = response.GetResponseStream();
+                    StreamReader srReadData = new StreamReader(stReadData, Encoding.UTF8);
+
+                    // 응답 Stream -> 응답 String 변환
+                    string strResult = srReadData.ReadToEnd();
+
+                    Console.WriteLine(strResult);
+                    Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    string t = ex.StackTrace;
+                }
+            }
+        }
+    }
+}
